24e6c13feda942542d0594255cf370c0898a1876
Si Truong <si.truongtan@gmail.com>
2021-12-19 22:42:40 +0700

    4. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken, 1000000);
+  var tokenPrice = 1000000000000000;
+  deployer.deploy(DappToken, 1000000).then(function() {
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
